30f36adaf9879e6ae675a5e44e9a1349e9baffd6
zhihaop <zhihaop@qq.com>
2022-08-16 18:21:17 +0800

    fix main.c

--- src/main.c
+++ src/main.c
@@ -8,18 +8,15 @@
 #include <sys/time.h>
 
 void foo(void *arg);
-
 void executorExample();
-
 void arrayBlockingQueueExample();
-
 void linkedBlockingQueueExample();
-
 void blockingQueueExample(BlockingQueue *queue, int queueSize);
 
 int main() {
     executorExample();
     linkedBlockingQueueExample();
+    arrayBlockingQueueExample();
 }
 
 void foo(void *arg) {
